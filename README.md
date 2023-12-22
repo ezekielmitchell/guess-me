d4970c943bc936ed7d7a314dbe98d68f5a9936ce
Ezekiel A. Mitchell <135088287+ezekielmitchell@users.noreply.github.com>
2023-12-21 19:20:08 -0800

    v0.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Guess-Me
+Ezekiel A. Mitchell (2023)
+___
+### A simple guessing game project made with rust.
+
+#### How to play:
+* Clone the repository
+* Run `cargo run` in the terminal
+* Enter your guess
+* Enjoy!
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+
+
+    println!("{}", number(100));
+
+}
+
+// generator number
+fn number(max: i32) -> i32 {
+    rand::thread_rng().gen_range(0..=max) // generates a random int between 0 and 'max' value
 }

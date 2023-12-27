d207550ea3c28214d383acbc43491f6a896475d1
Ezekiel A. Mitchell <135088287+ezekielmitchell@users.noreply.github.com>
2023-12-26 18:40:34 -0800

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 #![allow(unused)]
 
-use std::io;
+use std::io::stdin;
 use rand::Rng;
 
 fn main() {
+    let number = number(1);
 
+    loop {
+        setup();
+        let user_input = user_input();
 
-    println!("{}", number(100));
-
+        if user_input == number {
+            println!("You won! The correct number was : {}", user_input);
+            break
+        } else {
+            println!("Incorrect!\nYou entered : {}\n", &user_input);
+            continue
+        }
+    }
 }
 
-// generator number
+// generates a random int between 0 and 'max' value
 fn number(max: i32) -> i32 {
-    rand::thread_rng().gen_range(0..=max) // generates a random int between 0 and 'max' value
+    rand::thread_rng()
+        .gen_range(0..=max)
+}
+
+// read user input
+fn user_input() -> i32 {
+    let mut input = String::new();
+
+    stdin() // read user input
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    // parse input (String) -> input (int)
+    let result: i32 = input.trim().parse().expect("Invalid entry");
+
+    result // return user input as integer
+}
+
+fn setup() {
+    println!("Enter an integer between 1-100")
 }
